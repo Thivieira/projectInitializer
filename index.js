9c29d44c254ae83e384315b1d223dffbd0d276cb
Thiago Alves Vieira <thiago.a.vieira@hotmail.com>
2021-05-21 23:23:11 -0300

    commiteiro

--- index.js
+++ index.js
@@ -1,26 +1,24 @@
-const program = require('commander')
-const exec = require('child_process').exec
-
+const program = require("commander");
+const exec = require("child_process").exec;
 
 function cloneProject(url, path) {
-    exec(`git clone ${url} ${path}`,
-        function (err, stdout, stderr) {
-            if (err) throw err;
-            else console.log(stdout);
-        });
+  exec(`git clone ${url} ${path}`, function (err, stdout, stderr) {
+    if (err) throw err;
+    else console.log(stdout);
+  });
 }
-program
-    .version('0.0.1')
-    .description('Project Creator');
+program.version("0.0.1").description("Project Creator");
 
 program
-    .command('project <githubURL> <newPath>')
-    .alias('p')
-    .description('Add project')
-    .action((githubURL, newPath) => {
-        cloneProject(githubURL, newPath);
-    });
+  .command("project <githubURL> <newPath>")
+  .alias("p")
+  .description("Add project")
+  .action((githubURL, newPath) => {
+    cloneProject(githubURL, newPath);
+  });
 
 program.parse(process.argv);
 
 console.log("oi");
+
+console.log("sabrinha");
